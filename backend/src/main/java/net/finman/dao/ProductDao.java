77878d3f71c17acbb47b5e479247972d68222746
yenanw <54945977+yenanw@users.noreply.github.com>
2021-04-23 11:37:37 +0200

    added javadoc for the product interfaces
    Co-authored-by: Puh00 <54896147+Puh00@users.noreply.github.com>

--- backend/src/main/java/net/finman/dao/ProductDao.java
+++ backend/src/main/java/net/finman/dao/ProductDao.java
@@ -9,11 +9,41 @@ import net.finman.exception.ResourceNotUpdatedException;
 import net.finman.model.Item;
 
 public interface ProductDao {
+    /**
+     * Creates a product.
+     * 
+     * @param i Item to be stored in the database.
+     * @throws ResourceNotCreatedException If the sql insertion failed.
+     */
     void createProduct(Item i) throws ResourceNotCreatedException;
 
+    /**
+     * Retrieves all products related to the specified owner.
+     * 
+     * @param owner The account that owns the product.
+     * @return List of products.
+     * @throws ResourceNotFoundException If the sql query found nothing.
+     */
     List<Item> getProductsOwnedBy(int owner) throws ResourceNotFoundException;
 
+    /**
+     * Deletes a product from an account.
+     * 
+     * @param owner The account that owns the product.
+     * @param name The name of the product to be deleted.
+     * @throws ResourceNotDeletedException If the sql delete failed.
+     * @throws ResourceNotFoundException If the sql quety found nothing.
+     */
     void deleteProduct(int owner, String name) throws ResourceNotDeletedException, ResourceNotFoundException;
 
+    /**
+     * Updates a product.
+     * 
+     * @param owner The account that owns the product.
+     * @param name The name of the product to be updated.
+     * @param item The new product.
+     * @throws ResourceNotFoundException If the sql query found nothing.
+     * @throws ResourceNotUpdatedException If the sql update failed.
+     */
     void updateProduct(int owner, String name, Item item) throws ResourceNotFoundException, ResourceNotUpdatedException;
 }
