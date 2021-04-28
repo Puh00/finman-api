8424128a60aaa25bd0a89a89909a0e435c1e2289
yenanw <54945977+yenanw@users.noreply.github.com>
2021-04-28 15:44:19 +0200

    fix GET request for invoices
    Co-authored-by: luddebubb <ludvig.hollander@hotmail.com>

--- backend/src/main/java/net/finman/mapper/ItemMapper.java
+++ backend/src/main/java/net/finman/mapper/ItemMapper.java
@@ -12,7 +12,7 @@ public class ItemMapper implements RowMapper<Item> {
     @Override
     public Item mapRow(ResultSet rs, int rowNum) throws SQLException {
         Item item = new Item();
-        item.setOwner(rs.getInt("owner"));
+        item.setOwner(rs.getString("owner"));
         item.setName(rs.getString("name"));
         item.setPrice(rs.getInt("price"));
         item.setAmount(rs.getInt("amount"));
