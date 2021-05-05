6a38a9a63548249e2ddcdcee6d4507cc694deb33
yenanw <54945977+yenanw@users.noreply.github.com>
2021-05-05 08:49:20 +0200

    update database credentials and format code

--- backend/src/main/java/net/finman/controller/EmailController.java
+++ backend/src/main/java/net/finman/controller/EmailController.java
@@ -1,13 +1,5 @@
 package net.finman.controller;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-
-import javax.mail.MessagingException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,11 +22,12 @@ public class EmailController {
     private EmailService emailService;
 
     private static final String EMAIL_SUBJECT = "Invoice from A.Finman";
-    private static final String EMAIL_BODY = "Hello here is your invoice pls pay q:^)"; 
+    private static final String EMAIL_BODY = "Hello here is your invoice pls pay q:^)";
 
     @PostMapping("/send-invoice")
-	public ResponseEntity<?> sendInvoice(@RequestParam("file") MultipartFile file, @RequestParam("to") String to) throws EmailNotSentException {
+    public ResponseEntity<?> sendInvoice(@RequestParam("file") MultipartFile file, @RequestParam("to") String to)
+            throws EmailNotSentException {
         emailService.sendEmailWithAttachment(to, EMAIL_SUBJECT, EMAIL_BODY, file);
-		return new ResponseEntity<>(HttpStatus.OK);
-	}
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
